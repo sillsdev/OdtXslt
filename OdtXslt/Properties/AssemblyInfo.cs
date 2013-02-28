614a900bb91323cef7bc160a0728d521cca96c8c
Trihus <Trihus@TRIHUSG-1209.dallas.sil.org>
2013-02-28 14:05:48 -0600

    More adjustments for .net 3.5 (Windows XP)

--- OdtXslt/Properties/AssemblyInfo.cs
+++ OdtXslt/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("OdtXslt")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Apply XSLT to items inside ODT, EPUB, or DOCX")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Windows User")]
+[assembly: AssemblyCompany("SIL International")]
 [assembly: AssemblyProduct("OdtXslt")]
-[assembly: AssemblyCopyright("Copyright © Windows User 2012")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("© 2013 SIL International")]
+[assembly: AssemblyTrademark("SIL International")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.6.0.9")]
+[assembly: AssemblyFileVersion("1.6.0.9")]
